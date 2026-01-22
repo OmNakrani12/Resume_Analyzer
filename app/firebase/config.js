b2943d11d9c87a6f1afb30032372c4d9b93a31f4
OmNakrani12 <omnakrani971@gmail.com>
2026-01-23 01:45:04 +0530

    Authentication Done

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiuGtr4Rr2ShVtLYeJt7q9oLYgA0jmyyU",
-  authDomain: "resume-analyzer-b7199.firebaseapp.com",
-  projectId: "resume-analyzer-b7199",
-  storageBucket: "resume-analyzer-b7199.firebasestorage.app",
-  messagingSenderId: "602207671287",
-  appId: "1:602207671287:web:84c7f2775dbc3e9fc8d4f2",
-  databaseURL: "https://resume-analyzer-b7199-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  measurementId: "G-9RKHLQ8DGT"
+  apiKey: Process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  databaseURL: process.env.DATABASEURL,
+  measurementId: process.env.MEASUREMENTID,
 };
 
 // Initialize Firebase
